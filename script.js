5c9744658b1772d85e12677d420dd69d05b835fe
Jacqueline <deanstro08@gmail.com>
2022-03-09 20:04:26 -0600

    Edit script.js and style.css

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector(".container");
+const pencilBtn = document.getElementById("pencil");
+const eraserBtn = document.getElementById("eraser");
+const fillBtn = document.getElementById("fill");
+const eye_dropBtn = document.getElementById("eye-drop");
+const undoBtn = document.getElementById("undo");
+const redoBtn = document.getElementById("redo");
+const clearBtn = document.getElementById("clear");
+const gridDisplayBtn = document.getElementById("grid-display");
+ 
