4fff7ea812fdccc5a3b152721dee7ecc2ddab502
Jacqueline <deanstro08@gmail.com>
2022-03-10 20:23:37 -0600

    Add Grid and ButtonStyling

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ const redoBtn = document.getElementById("redo");
 const clearBtn = document.getElementById("clear");
 const gridDisplayBtn = document.getElementById("grid-display");
  
+
+const createGrid = (size) => {
+    let gridTotal = size ** 2;
+    
+    container.style.gridTemplateColumns =  `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows =  `repeat(${size}, 1fr)`;
+    
+    
+    for (let i = 0; i < gridTotal; i++) {
+         let square = document.createElement("div");
+         square.style.backgroundColor = "white";
+         container.insertAdjacentElement("beforeend", square);
+        };
+   };
+
+createGrid(16);
+
